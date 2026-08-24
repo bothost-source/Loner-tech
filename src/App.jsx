7c8a5bb005d34fb5eeddec02ccd87c41574e4c25
bothost-source <destinyjob493@gmail.com>
2026-08-24 12:28:33 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
 import Skills from './components/Skills.jsx';
 import Services from './components/Services.jsx';
+import Projects from './components/Projects.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
+import CustomCursor from './components/CustomCursor.jsx';
 
 export default function App() {
   return (
     <>
+      <CustomCursor />
       <Header />
       <Hero />
       <Skills />
       <Services />
+      <Projects />
       <About />
       <Contact />
       <Footer />
     </>
   );
 }
+ 
